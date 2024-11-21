fa8b88f89f54ca3e9ece332814e25e13c4887836
Devang <devang@prismitsystems.com>
2024-11-21 17:48:23 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ const Card = ({ title, year, img }: CardType) => {
         <div className='w-full px-[8px] pt-[8px] pb-[16px] rounded-lg bg-cardColor hover:bg-inputColor'>
 
             <div >
-                <Image width={100} height={100} src={`${img}`} alt="Card Image" className='w-full h-[246px] sm:h-[400px] rounded-lg object-cover' />
+                <img src={`${img}`} alt="Card Image" className='w-full h-[246px] sm:h-[400px] rounded-lg object-cover' />
             </div>
             <div className='mt-4'>
                 <h2 className='mb-2 font-semibold'>{title}</h2>
